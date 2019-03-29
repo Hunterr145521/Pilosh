ad6c48a3f6e35837ad34d8807855f6553c9bae42
AshutoshGupta97 <guptashu.1997@gmail.com>
2019-03-29 23:32:35 +0530

    Corrected Auth

--- App.js
+++ App.js
@@ -67,18 +67,22 @@
 // };
 //
 // export default connect(mapStateToProps, mapDispatchToProps)(App);
- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////;
- import { Navigation } from "react-native-navigation";
 
- import AuthScreen from "./src/screens/Auth/Auth";
+import { Navigation } from "react-native-navigation";
 
- // Register Screens
- Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
 
- // Start a App
- Navigation.startSingleScreenApp({
-   screen: {
-     screen: "awesome-places.AuthScreen",
-     title: "Login"
-   }
- });
+// Register Screens
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
+
+// Start a App
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesome-places.AuthScreen",
+    title: "Login"
+  }
+});
