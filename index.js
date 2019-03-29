29ecc5fbc29c24dda93e77147a65086aa26f922e
Hunterr145521 <s.satyamsingh17@gmail.com>
2019-03-29 20:13:44 +0530

    New Changes

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import { Provider } from 'react-redux';
+// import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import configureStore from './src/store/configureStore';
-const store = configureStore();
-const RNRedux = () => (
-  <Provider store = {store}>
-    <App />
-  </Provider>
-);
-AppRegistry.registerComponent(appName, () => RNRedux);
+// import {name as appName} from './app.json';
+//
+// AppRegistry.registerComponent(appName, () => RNRedux);
