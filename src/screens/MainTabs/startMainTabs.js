acd86580e3d77a024d2643a423c0875fc435e1e2
Hunterr145521 <s.satyamsingh17@gmail.com>
2019-03-30 00:37:46 +0530

    Updated

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,21 +1,29 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const startTabs = () => {
-
-        Navigation.startTabBasedApp({
-            tabs: [
-                {
-                    screen: "awesome-places.FindPlaceScreen",
-                    label: "Find Place",
-                    title: "Find Place"
-                },
-                {
-                    screen: "awesome-places.SharePlaceScreen",
-                    label: "Share Place",
-                    title: "Share Place"
-                }
-            ]
+  Promise.all([
+          Icon.getImageSource("streetview", 30, "blue"),
+          Icon.getImageSource("share", 30 , "green")
+        ]).then(source => {
+          Navigation.startTabBasedApp({
+              tabs: [
+                  {
+                      screen: "awesome-places.FindPlaceScreen",
+                      label: "Find Place",
+                      title: "Find Place",
+                      icon: source[0]
+                  },
+                  {
+                      screen: "awesome-places.SharePlaceScreen",
+                      label: "Share Place",
+                      title: "Share Place",
+                      icon: source[1]
+                  }
+              ]
+          });
         });
+
     };
 //whatis that
 export default startTabs;
