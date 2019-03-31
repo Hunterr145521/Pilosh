dbd7c1ac52e6fc18c8b893348a44e1b033292089
AshutoshGupta97 <guptashu.1997@gmail.com>
2019-04-01 00:13:19 +0530

    Log out

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
-import {Text, View, Dimensions, StyleSheet} from 'react-native';
-
+import {Text, View, Dimensions, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 class SideDrawer extends Component {
   render(){
     return(
-      <View style={[styles.container, {width: Dimensions.get("window").width * 0.7}]}><Text>SideDrawer</Text></View>
+      <View style={[styles.container, {width: Dimensions.get("window").width * 0.7}]}>
+      <TouchableOpacity>
+      <View>
+      <Icon name="arrow_back_ios" size={30} color="#aaa"/>
+      <Text>Sign Out</Text>
+      </View>
+      </TouchableOpacity>
+      </View>
     );
   }
 }
