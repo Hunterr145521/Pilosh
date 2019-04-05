5f85ee6dc1e3f58cd4a905446093984f85b9e7ac
AshutoshGupta97 <guptashu.1997@gmail.com>
2019-04-05 22:30:28 +0530

    Map_try_06_success

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import {View, Image, Button, StyleSheet, Text, Dimensions} from "react-native";
+import MapView from "react-native-maps";
 
 class PickLocation extends Component {
+  state = {
+    focusedLocation:{
+      latitude: 27.096400,
+      longitude: 76.286200,
+      latitudeDelta: 0.0122,
+      longitudeDelta:
+        Dimensions.get("window").width / Dimensions.get("window").height * 0.0122
+
+    }
+  }
   render(){
     return(
       <View style={styles.container}>
-      <View style={styles.placeholder}>
-        <Text>Map</Text>
-      </View>
+      <MapView
+        initialRegion={this.state.focusedLocation}
+        style={styles.map}
+      />
         <View style={styles.button}>
           <Button title ="Locate Me!" onPress={() => alert("Where is this palce!")} />
         </View>
@@ -19,14 +31,11 @@ class PickLocation extends Component {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-  alignItems:"center"  }
-  ,
-placeholder: {
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor:"#eee",
-    width:"80%",
-    height:150
+  alignItems:"center"
+  },
+  map: {
+    width:"100%",
+    height:250
   },
   button:{
     margin:7
