cff4b557b6019c019b58e0fde0ae48a22c09cf6a
Hunterr145521 <s.satyamsingh17@gmail.com>
2019-04-14 23:02:22 +0530

    Deployying!!!

--- android/app/src/main/java/com/pilosh/MainActivity.java
+++ android/app/src/main/java/com/pilosh/MainActivity.java
@@ -2,6 +2,27 @@ package com.pilosh;
 
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+public class MainActivity extends SplashActivity {
+@Override
+  public LinearLayout createSplashLayout() {
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
 
-public class MainActivity extends SplashActivity { 
+    view.setBackgroundColor(Color.parseColor("#00E676"));
+    view.setGravity(Gravity.CENTER);
+
+    textView.setTextColor(Color.parseColor("#039BE5"));
+    textView.setText("PiLoSh");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+    view.addView(textView);
+    return view;
+  }
+
 }
